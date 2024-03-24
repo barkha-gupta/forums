955bbda6d1afb9b01d977d24fc778d981ed6eada
barkha-gupta <eshkon.barkha@gmail.com>
2024-03-24 23:17:25 +0530

    feat: add side bars for layout

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,3 +1,14 @@
 export const RightSidebar = () => {
-  return <div>RightSidebar</div>;
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="felx flex-1 flex-col justify-start">
+        <h3 className="text-light-1 text-heading4-medium">
+          Suggested Communities
+        </h3>
+      </div>
+      <div className="felx flex-1 flex-col justify-start">
+        <h3 className="text-light-1 text-heading4-medium">Suggested Users</h3>
+      </div>
+    </section>
+  );
 };
