5812a7aaf68108cc4137ffb6dbe9562fe68fd8cb
barkha-gupta <eshkon.barkha@gmail.com>
2024-04-08 00:38:45 +0530

    feat: forum card

---
+++ app/(root)/forum/[id]/page.tsx
@@ -0,0 +1,5 @@
+const page = ({ params }: { params: { id: string } }) => {
+  return <div>page</div>;
+};
+
+export default page;
--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,18 +1,42 @@
 import { fetchForums } from "@/lib/actions/forum.action";
+import ForumCard from "./cards/ForumCard";
+import { currentUser } from "@clerk/nextjs";
 
 export const HomePage = async () => {
   const { posts } = await fetchForums(1, 30);
+  const user = await currentUser();
 
   return (
     <>
       <h1 className="head-text text-left"> Home</h1>;
-      <div>
+      {/* <div>
         {posts.map((post) => (
           <p key={post._id} className="text-light-1">
             {post.text}
           </p>
         ))}
-      </div>
+      </div> */}
+      <section className="mt-9 flex flex-col gap-10">
+        {posts.length === 0 ? (
+          <p className="no-result">No bullets found!</p>
+        ) : (
+          <>
+            {posts.map((post) => (
+              <ForumCard
+                key={post.id}
+                id={post.id}
+                currentUserId={user?.id || ""}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.comments}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </>
   );
 };
