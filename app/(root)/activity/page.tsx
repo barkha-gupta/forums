357b914784c61106e70e7123806e312bf2424769
barkha-gupta <eshkon.barkha@gmail.com>
2024-04-15 18:43:48 +0530

    fix: layout for auth forms

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,5 +1,22 @@
-const Page = () => {
-  return <div>page</div>;
+import { getUser } from "@/lib/actions/user.action";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await getUser(user.id);
+  if (!userInfo) redirect("/onboarding");
+
+  return (
+    <section>
+      <h1 className="head-text mb-10">Search</h1>
+      <p className="text-samll-medium text-gray-1">
+        All activities will be shown here!
+      </p>
+    </section>
+  );
 };
 
 export default Page;
