820622383e33f34704ad3d32349bfeba2872116b
barkha-gupta <eshkon.barkha@gmail.com>
2024-04-15 19:16:28 +0530

    fix: onboarding

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,9 +1,15 @@
 import { Accountprofile } from "@/components/forms/Accountprofile";
+import { getUser } from "@/lib/actions/user.action";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
   const user = await currentUser();
-  const userInfo = {};
+  if (!user) return null;
+
+  const userInfo = await getUser(user.id);
+  if (userInfo?.onboard) redirect("/");
+
   const userData = {
     id: user?.id,
     objectId: userInfo?._id,
